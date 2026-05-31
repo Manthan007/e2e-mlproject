e2baf535b632cff5dab94c020496878cfc0a58ae
Manthan007 <manthansharma7818@gmail.com>
2026-06-01 04:56:56 +0530

    model trainer

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
 
 import sys
 
@@ -16,8 +18,15 @@ if __name__=="__main__":
 
         # data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+        ## Model training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
     except Exception as e:
-        logging.info("Custom Exception")
-        raise CustomException(e, sys)
+            logging.error(f"Error occurred: {str(e)}")
+            # This forces Python to print the raw, ugly traceback directly to your terminal screen
+            import traceback
+            traceback.print_exc()
+            raise e
--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -16,6 +16,7 @@ from xgboost import XGBRegressor
 
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
+from src.mlproject.utils import save_object, evaluate_models
 
 
 @dataclass
@@ -84,5 +85,29 @@ class ModelTrainer:
                 
             }
 
+            model_report: dict = evaluate_models(X_train, y_train, X_test, y_test, models, params)
+
+            # Get the best model score and name safely
+            best_model_name = max(model_report, key=model_report.get)
+            best_model_score = model_report[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            
+            best_model = models[best_model_name]
+            
+            logging.info(f"Best model found: {best_model_name} with score: {best_model_score}")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_scoree = r2_score(y_test, predicted)
+            
+            return r2_scoree
+
         except Exception as e:
             raise CustomException(e, sys)
+        
